a8af411182f6689e8c67d1eaa34d5a28dae73836
Kevin Wu <kevinwu.858@gmail.com>
2024-04-06 21:55:52 -0700

    add location

--- src/components/UserToasts/ToastList.js
+++ src/components/UserToasts/ToastList.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ToastCard from './ToastCard';
 
 export default function ToastList({ toasts }) {
-    console.log(toasts);
+    console.log('toasts', toasts);
     return(
         <div className="px-16">
             {
                 toasts && toasts.map((t) => {
-                    return <ToastCard id={t._id} name={t.toasteeName} location={t.location} timestamp={t.updatedAt} gear={t.arcteryxProduct} likes={t.likes} username={t.toasterName} description={t.content} />
+                    return <ToastCard id={t._id} name={t.toasteeName} location={t.arcteryxStore} timestamp={t.updatedAt} gear={t.arcteryxProduct} likes={t.likes} username={t.toasterName} description={t.content} />
                 })
             }
         </div>
