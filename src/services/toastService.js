70bc2c0b91f954e481204dc8b96a12f657326dec
Kevin Wu <kevinwu.858@gmail.com>
2024-04-07 10:35:30 -0700

    use service

--- src/services/toastService.js
+++ src/services/toastService.js
@@ -2,13 +2,13 @@ import axios from 'axios';
 const token = localStorage.getItem('jwt');
 
 export const getToasts = async () => {
-    const toasts = await axios.get('http://localhost:3001/toast', { headers: { Authorization: `Bearer ${token}` } });
+    const toasts = await axios.get('https://toasters-service.onrender.com/toast', { headers: { Authorization: `Bearer ${token}` } });
     return toasts.data;
 };
 
 export const createToast = async (toastData) => {
     try {
-        const response = await axios.post('http://localhost:3001/toast', toastData, { headers: { Authorization: `Bearer ${token}` } });
+        const response = await axios.post('https://toasters-service.onrender.com/toast', toastData, { headers: { Authorization: `Bearer ${token}` } });
         return response.data;
     } catch (error) {
         console.error('Error creating toast:', error);
@@ -17,5 +17,5 @@ export const createToast = async (toastData) => {
 };
 
 export const likeToast = async (id, like, userId) => {
-    await axios.patch(`http://localhost:3001/toast/${id}`, { userId, like }, { headers: { Authorization: `Bearer ${token}` } });
+    await axios.patch(`https://toasters-service.onrender.com/toast/${id}`, { userId, like }, { headers: { Authorization: `Bearer ${token}` } });
 };
--- src/services/userService.js
+++ src/services/userService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const validateUser = async (username, password) => {
     try {
-        const res = await axios.post('http://localhost:3001/auth', { username, password });
+        const res = await axios.post('https://toasters-service.onrender.com/auth', { username, password });
         const jwt = res.data.token;
         localStorage.setItem('jwt', jwt);
     } catch (e) {
@@ -12,7 +12,7 @@ export const validateUser = async (username, password) => {
 
 export const createNewUser = async (data) => {
     try {
-        const res = await axios.post('http://localhost:3001/user', data);
+        const res = await axios.post('https://toasters-service.onrender.com/user', data);
         const jwt = res.data.token;
         localStorage.setItem('jwt', jwt);
     } catch (e) {
